6cd3636a9135c51ab35df67e8fbf4f5df8423939
LeonMarqs <leomarques301@gmail.com>
2021-06-22 14:42:47 -0300

    conexao com banco de dados sqlite e criacao de usuario

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
-import 'reflect-meta-data';
-import express from "express";
+import 'reflect-metadata';
+import express, { json } from "express";
+import { router } from './routes';
+
+import './database';
 
 const app = express();
 
+app.use(express.json());
+
+app.use(router);
+
 app.listen(3000, () => console.log("Server is running"));
-
-app.get("/test" , (request, response) => {
-  return response.send("Hello");
-});
-
